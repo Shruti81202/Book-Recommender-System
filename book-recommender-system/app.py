607a1d900fce10e2d57facf3874dbe408da221db
Shruti81202 <121923425+Shruti81202@users.noreply.github.com>
2023-07-12 16:01:05 +0530

    Update app.py

--- book-recommender-system/app.py
+++ book-recommender-system/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pickle
 import numpy as np
-
+import pandas as pd
 popular_df = pickle.load(open('popular.pkl', 'rb'))
 pt = pickle.load(open('pt.pkl', 'rb'))
 books = pickle.load(open('books.pkl', 'rb'))
